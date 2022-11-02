8288bf1c0ef15fd334d58596231adb1a26ef44d7
govindhansv <96695721+govindhansv@users.noreply.github.com>
2022-11-02 19:14:27 +0530

    port err fixed

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get('/', function(req, res) {
     res.send('Welcome Cowboys!');
 })
 
-var server = app.listen(8081, function() {
+var server = app.listen(process.env.PORT || 8081, function() {
     var host = server.address().address
     var port = server.address().port
     console.log(`Example app listening at http://localhost:${port}`)
